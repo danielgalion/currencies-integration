454415d268381281de0e1bb50096c45fae3374ee
Daniel Galion <daniel.galion94@gmail.com>
2023-04-11 17:52:47 +0200

    Make currencies table editable
    Add fields into fillable array

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Currency extends Model {
     protected $table = 'currencies';
+    protected $fillable = ['name', 'currency_code', 'exchange_rate'];
 }
